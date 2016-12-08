19e43d7e947e452a2c61dde8717f0d0ed8a7b818
Tim Court <tim.court@enbridge.com>
2016-12-08 16:05:41 -0700

    all tasks complete

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/tctimmeh.json',
+    dataType: 'jsonp',
+    success: (data) => {
+      let badges = $('#badges');
+      for (let course of data.courses.completed) {
+        badges.append($(`<div class="course">
+          <h3>${course.title}</h3>
+          <img src="${course.badge}" />
+          <a href="${course.url}" class="btn btn-primary" target="_blank">See Course</a>
+        </div>`));
+      }
+    }
+  });
 
 });
